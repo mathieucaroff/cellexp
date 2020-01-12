20b0adcbbf0d637e02d2bb24aa545dcd63845aaf
Mathieu CAROFF <mathieu.caroff@free.fr>
2020-01-12 08:51:12 +0100

    control (ResetTime.tsx change): add disabled
    ResetTime appears disabled when useless

--- src/ui/control/components/ResetTime.tsx
+++ src/ui/control/components/ResetTime.tsx
@@ -10,13 +10,17 @@ export let ResetTime = observer(() => {
    let store = useStore()
 
    let handleClick = action(() => {
+      store.play = false
+
       store.posT.wholePos = 0
       store.posT.microPos = 0
    })
 
+   let isZero = store.posT.wholePos === 0 && store.posT.microPos === 0
+
    return (
       <div>
-         <Button variant="outlined" onClick={handleClick}>
+         <Button variant="outlined" disabled={isZero} onClick={handleClick}>
             Reset
          </Button>
       </div>
