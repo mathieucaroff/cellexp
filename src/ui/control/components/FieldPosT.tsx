3e4ed6308a00e4c0563c1be3d94e8199d3dca667
Mathieu CAROFF <mathieu.caroff@free.fr>
2020-01-12 20:40:27 +0100

    control (FieldPosT.tsx fix): valid limit overrun
    - Stop complaining when the application reaches a state
      the user is not allowed to set.
    - Raise the limit from 9_999 to 99_999

--- src/ui/control/components/FieldPosT.tsx
+++ src/ui/control/components/FieldPosT.tsx
@@ -6,21 +6,27 @@ import { useStore } from '../../util/useStore'
 import { errorCheck } from '../../../util/errorCheck'
 import { SelectorInput } from './SelectorInput'
 
-let validation = (value: string) => {
-   let notAnInteger = () => !value.match(/^-?\d*$/)
-   let notPositive = () => !!value.match(/^-/)
-   let tooLong = () => value.length > 4
-
-   return errorCheck(
-      'Elapsed generations (time)',
-      [notAnInteger, 'Generation must be an integer'],
-      [notPositive, 'Generation must be positive'],
-      [tooLong, 'Generation must between 0 and 9999'],
-   )
-}
-
 export let FieldPosT = observer(() => {
    let store = useStore()
+   let { wholePos } = store.posT
+
+   let validation = (value: string): [boolean, string] => {
+      let defaultHelp = 'Elapsed generations (time)'
+      if (value === '' + wholePos) {
+         return [false, defaultHelp]
+      }
+
+      let notAnInteger = () => !value.match(/^-?\d*$/)
+      let notPositive = () => !!value.match(/^-/)
+      let tooLong = () => value.length > 5
+
+      return errorCheck(
+         defaultHelp,
+         [notAnInteger, 'Generation must be an integer'],
+         [notPositive, 'Generation must be positive'],
+         [tooLong, 'Generation must between 0 and 99999'],
+      )
+   }
 
    return (
       <SelectorInput
