293feb20423a2bd4101c664f370dd3d86fa61a29
Mathieu CAROFF <mathieu.caroff@free.fr>
2020-01-19 03:04:06 +0100

    ui (Rule.tsx change): css link with pointer

--- src/ui/editor/components/Rule.tsx
+++ src/ui/editor/components/Rule.tsx
@@ -1,19 +1,39 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core'
 import { Link } from '@material-ui/core'
 import { action } from 'mobx'
 import { observer } from 'mobx-react-lite'
 import * as React from 'react'
 import { useStore } from '../../util/useContextHook'
 
+let useStyle = makeStyles((theme: Theme) => {
+   return createStyles({
+      fakeHref: {
+         cursor: 'pointer',
+      },
+   })
+})
+
 export interface RuleProp {
    rule: number
 }
 
 export let Rule = observer((prop: RuleProp) => {
+   let classes = useStyle()
    let store = useStore()
    let onClick = action(() => {
       store.rule = prop.rule
    })
    let text = `rule ${prop.rule}`
-   let eq = store.rule === prop.rule
-   return <>({eq ? text : <Link onClick={onClick}>{text}</Link>})</>
+
+   let content
+   if (store.rule === prop.rule) {
+      content = text
+   } else {
+      content = (
+         <Link className={classes.fakeHref} onClick={onClick}>
+            {text}
+         </Link>
+      )
+   }
+   return <>({content})</>
 })
