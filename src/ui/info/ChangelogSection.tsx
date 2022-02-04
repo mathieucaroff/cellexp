7ede41cf13185f85bcc4d210328abc8e06e4cebc
Mathieu CAROFF <mathieu.caroff@free.fr>
2022-02-04 06:19:15 +0100

    project (/*): text file inlining

--- src/ui/info/ChangelogSection.tsx
+++ src/ui/info/ChangelogSection.tsx
@@ -1,14 +1,11 @@
 import Link from '@material-ui/core/Link'
-import fs from 'fs'
 import { observer } from 'mobx-react-lite'
 import * as React from 'react'
 import { md2react } from './md2react'
 import { usePromise } from './usePromise'
 
-const markdownContent = fs.readFileSync(
-   __dirname + '../../../../CHANGELOG.md',
-   'utf-8',
-)
+// @ts-ignore
+import markdownContent from "bundle-text:../../../CHANGELOG.md";
 
 let promise = md2react(markdownContent, {
    a: Link,
